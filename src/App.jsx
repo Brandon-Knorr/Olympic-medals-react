ed578f3a5bb0ce7ab0fda707483a3b3d9b071a4e
Brandon-Knorr <bknorr@my.wctc.edu>
2025-08-26 20:39:25 -0500

    assignment 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import './App.css'
-import Country from './components/Country'
+import "./App.css";
+import Country from "./components/Country";
+import { useState } from "react";
 
 function App() {
+	const [countries, setCountries] = useState([
+		{ id: 1, name: "United States", gold: 2 },
+		{ id: 2, name: "China", gold: 3 },
+		{ id: 3, name: "France", gold: 0 },
+	]);
 
+	function handleDelete(countryId) {
+		console.log(`delete word: ${countryId}`);
+		setCountries(countries.filter((c) => c.id !== countryId));
+	}
 
-  return (
-    <>
-   <h1>Welcome To My Olympic Medal App!</h1>
-   <Country/>
-  </>)
+	return (
+		<>
+			<h1>Welcome To My Olympic Medal App!</h1>
+			{countries.map((country) => (
+				<Country
+					key={country.id}
+					country={country}
+					onDelete={handleDelete}
+				/>
+			))}
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,21 +1,29 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-function Country() {
-    const [text, setText] = useState("United States");
-    const [count, setCount] = useState(0);
+function Country(props) {
+	// const [text, setText] = useState("United States");
+	// const [count, setCount] = useState(0);
 
-    function handleClick() {
-        setCount((count) => count + 1);
-    }
+	// function handleClick() {
+	//     setCount((count) => count + 1);
+	// }
 
-    return (
-        <>
-            <h3>
+	//MAKING THE CHANGES FOR THE SECOND ASSIGNMENT.
+
+	return (
+		<>
+			{/* <h3>
                 {text} : {count}{" "}
             </h3>
-            <button onClick={handleClick}>+</button>
-        </>
-    );
+            <button onClick={handleClick}>+</button> */}
+
+			<h4 className="country">
+				<u>{props.country.name}</u>
+			</h4>
+			<h4 className="country">{props.country.gold}</h4>
+			<button onClick={() => props.onDelete(props.country.id)}>X</button>
+		</>
+	);
 }
 
 export default Country;
